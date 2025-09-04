aa00895d4b6d38b00910318e865ad2befa9ced00
ACT-STUDENT <ACT-STUDENT@LAB4-PC43>
2025-09-04 13:35:33 +0800

    file

---
+++ EXER_03 User Input with Constructor/EXER_03 User Input with Constructor/Manwha.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_03_User_Input_with_Constructor
+{
+    internal class Manwha
+    {
+        //Declaring the fields
+        public string title;
+        public string genra;
+        public int yearofRelease;
+
+        public Manwha(string aTitle, string aGenra, int year) //Parestirized Constrictor
+        {
+            title = aTitle;
+            genra = aGenra;
+            yearofRelease = year;
+        }
+
+        //Method to display the book details
+
+        public void DisplayMawhaDetails()
+        {
+            Console.Clear();
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("-------Here are the Manwha Details");
+            Console.WriteLine("Manwha Title: " + title);
+            Console.WriteLine("Manwha Genra: " + genra);
+            Console.WriteLine("Year of Release: " + yearofRelease);
+        }
+    }
+}
---
+++ EXER_03 User Input with Constructor/EXER_03 User Input with Constructor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_03_User_Input_with_Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //John Rhaezel C. Atillo
+        //IT-306
+        //EXER_03: User Input with Constructor
+
+        Main:
+
+            Console.Clear();
+            //Ask the user to input values
+            Console.WriteLine("This program will ask about your favorite Manwha.\n\n");
+            Console.Write("Enter title of the manwha: \t\t");
+            string aTitle = Console.ReadLine();
+
+            Console.Write("Enter the genra of the manwha: \t\t");
+            string aGenra = Console.ReadLine();
+
+            Console.Write("Enter the year of release: \t\t");
+            int year = int.Parse(Console.ReadLine());
+
+            //Initialize a class object 
+            Manwha manwha1 = new Manwha(aTitle, aGenra, year);
+
+            //Call the method to display
+            manwha1.DisplayMawhaDetails();
+
+            //Input another book details
+            Console.Write("\nDo you want to try again (y/n)? ");
+            char ans = Convert.ToChar(Console.ReadLine());
+            if (ans == 'y')
+            {
+                goto Main;
+            }
+            else if (ans == 'n')
+            {
+                return;
+            }
+        }
+    }
+}
