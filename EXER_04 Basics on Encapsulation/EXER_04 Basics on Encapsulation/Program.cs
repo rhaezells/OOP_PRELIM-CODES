cd00cdc8f5255fa5bc04b29d616ad37d575a30fd
ACT-STUDENT <ACT-STUDENT@LAB4-PC43>
2025-09-04 13:27:14 +0800

    File

---
+++ EXER_04 Basics on Encapsulation/EXER_04 Basics on Encapsulation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_04_Basics_on_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //John Rhaezel C. Atillo
+            //IT 306
+            //EXER_04: Basics on Encapsulation
+
+            //Instantiate Shoes Object
+            Shoe shoe1 = new Shoe();
+
+            //Use encapsulated properties to set values
+            shoe1.Brand = "KQ";
+            shoe1.Size = 42;
+
+            //Call the method
+            shoe1.DisplayDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EXER_04 Basics on Encapsulation/EXER_04 Basics on Encapsulation/Shoe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_04_Basics_on_Encapsulation
+{
+    internal class Shoe
+    {
+        //Private Fields
+        private string brand;
+        private int size;
+
+        //Public Properties
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        //Method To Display
+        public void DisplayDetails()
+        {
+            Console.WriteLine("----Here are the Shoe Details----");
+            Console.WriteLine("Brand: " + Brand);
+            Console.WriteLine("Size: "+ Size);
+        }
+        
+    }
+}
