6b3156c98e6ff6086978780d3bd27de2ba4e2bec
ACT-STUDENT <ACT-STUDENT@LAB3-PC33>
2025-09-08 14:32:50 +0800

    File

---
+++ EXER_02  Create a Class/EXER_02  Create a Class/Manwha.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_02__Create_a_Class
+{
+    internal class Manwha
+    {
+        public string title, genra;
+        public int yearofRelease;
+
+        public Manwha()
+        {
+            title = "Blue lock";
+            genra = "Sports, Drama";
+            yearofRelease = 2018;
+
+            title = "Solo Leveling";
+            genra = "Action, Fantasy";
+            yearofRelease = 2016;
+
+            title = "The Beginning After The End";
+            genra = "Action, Adventure";
+            yearofRelease = 2013;
+        }
+    }
+}
---
+++ EXER_02  Create a Class/EXER_02  Create a Class/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_02__Create_a_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //John Rhaezel C. Atillo
+            //IT 306
+            //Exercise 2: Create a Class
+
+            Manwha manwha1 = new Manwha();
+            Manwha manwha2 = new Manwha();
+            Manwha manwha3 = new Manwha();
+
+            //Initialization values for the fields
+            manwha1.title = "Blue lock";
+            manwha1.genra = "Sports, Drama";
+            manwha1.yearofRelease = 2018;
+
+            manwha2.title = "Solo Leveling";
+            manwha2.genra = "Action, Fantasy";
+            manwha2.yearofRelease =2016;
+
+            manwha3.title = "The Beginning After The End";
+            manwha1.genra = "Action, Adventure";
+            manwha3.yearofRelease = 2013;
+
+            //call the method
+            Console.WriteLine("My anime List");
+            Console.WriteLine("Title: "+ manwha1.title +  "\n" + "Genra: " + manwha1.genra + "\n" + "Year of Release: " + manwha1.yearofRelease + "\n");
+            Console.WriteLine("Title: " + manwha2.title + "\n" + "Genra: " + manwha2.genra + "\n" + "Year of Release: " + manwha2.yearofRelease + "\n");
+            Console.WriteLine("Title: " + manwha3.title + "\n" + "Genra: " + manwha3.genra + "\n" + "Year of Release: " + manwha3.yearofRelease + "\n");
+
+            Console.ReadLine();
+        }
+    }
+}
