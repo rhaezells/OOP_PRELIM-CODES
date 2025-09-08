c658b328f8532d7c39ee0e1aa9963db4f5d4320c
ACT-STUDENT <ACT-STUDENT@LAB3-PC33>
2025-09-08 14:41:28 +0800

    file

---
+++ EXER_01 Creating a Class/EXER_01 Creating a Class/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_01_Creating_a_Class
+{
+    internal class Person
+    {
+        //Class members
+        public string name, gender;
+        public int age;
+
+        //Methods for display
+        public void Greet()
+        {
+            Console.WriteLine(" Hi Everyone, My name is " + name + ", "+ +age+ " years old " +  "and my gender is " + gender);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EXER_01 Creating a Class/EXER_01 Creating a Class/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_01_Creating_a_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            //Initialition of fields
+            person1.name = "John";
+            person1.age = 25;
+            person1.gender = "Male";
+
+            person1.Greet();
+        }
+    }
+}
